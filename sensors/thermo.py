6853ef53c8539d71cf1980a2bff5bf55a88fce1a
Mason <masonnixon@gmail.com>
2016-09-11 15:45:39 -0500

    Added device number to output.

--- sensors/thermo.py
+++ sensors/thermo.py
@@ -11,14 +11,18 @@ device_file = device_folder + '/w1_slave'
  
 def read_temp_raw(index):
     device_folder = glob.glob(base_dir + '28*')[index]
+    sensors = device_folder[len(base_dir):]
     device_file = device_folder + '/w1_slave'
     f = open(device_file, 'r')
     lines = f.readlines()
-    f.close()
-    return lines
+#    for i in range(0,len(sensors)-1):
+#        print sensors
+#    f.close()
+    return lines, sensors
  
 def read_temp(index):
-    lines = read_temp_raw(index)
+    retVals = read_temp_raw(index)
+    lines = retVals[0]
     while lines[0].strip()[-3:] != 'YES':
         time.sleep(0.2)
         lines = read_temp_raw()
@@ -27,12 +31,13 @@ def read_temp(index):
         temp_string = lines[1][equals_pos+2:]
         temp_c = float(temp_string) / 1000.0
         temp_f = temp_c * 9.0 / 5.0 + 32.0
-        return temp_f
+        return retVals[1], temp_f
 	
 while True:
 	numSensors = len(glob.glob(base_dir + '*'))
 	for i in range(0,numSensors-1):
+            tempVals = read_temp(i)
 	#	print i
-		print "Sensor ",i,": ",read_temp(i),"F "	
+	    print "Sensor ",i," (Device: ",tempVals[0],"): ",tempVals[1],"F "	
 	print "\n"
 	time.sleep(1)
