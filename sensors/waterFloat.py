c3a50773add8ddb822aad86e310e1eb5de6e85ca
Mason <masonnixon@gmail.com>
2016-10-08 20:33:11 -0500

    Got the water float working on BCM pin 23 and 3.3V.

--- sensors/waterFloat.py
+++ sensors/waterFloat.py
@@ -1,1 +1,25 @@
+#!/usr/bin/env python
+#coding: utf8
+
 import RPi.GPIO as GPIO
+#set up GPIO using BCM numbering
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(23, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+#GPIO.remove_event_detect(23)
+
+#def printFunction(channel):
+#  print('Water switch pressed!')
+#  print('Note how the bouncetime affects the button press')
+#  GPIO.add_event_detect(23, GPIO.RISING, callback=printFunction, bouncetime=300)
+
+while True:
+#  printFunction(0)
+  #if(GPIO.input(23) ==1):
+  #  print('Button 1 pressed')
+  GPIO.wait_for_edge(23, GPIO.RISING)
+  print('Water level is too low')
+  GPIO.wait_for_edge(23, GPIO.FALLING)
+  print('Water level is high enough')
+
+GPIO.cleanup()
+GPIO.remove_event_detect(23)
